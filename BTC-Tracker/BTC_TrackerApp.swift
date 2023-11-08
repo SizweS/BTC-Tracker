ebd6934b6134e608d73677f683250e6c4f92d07a
Sizwe Maluleke <sizwe.maluleke@byteorbit.com>
2023-11-08 15:13:36 +0200

    Initial Commit

---
+++ BTC-Tracker/BTC_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTC_TrackerApp.swift
+//  BTC-Tracker
+//
+//  Created by Sizwe Maluleke on 2023/11/08.
+//
+
+import SwiftUI
+
+@main
+struct BTC_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTC-Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BTC-Tracker
+//
+//  Created by Sizwe Maluleke on 2023/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
